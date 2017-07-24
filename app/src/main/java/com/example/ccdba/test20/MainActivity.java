cf2f89444f77e22ff9cf4b6c45a1e4bb44c3a2bd
kelvinngshuhui2016 <kelvinngshuhui2016@gmail.com>
2017-07-24 14:54:08 +0800

    測試2.1

--- app/src/main/java/com/example/ccdba/test20/MainActivity.java
+++ app/src/main/java/com/example/ccdba/test20/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        double test=2.0;
+        double test=2.1;
     }
 }
